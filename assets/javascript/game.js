b59bdf601d1155f6bbad5018e0a714066621db9d
Brucimus <Brucimus@gmail.com>
2018-06-17 12:24:12 -0700

    placed character objects

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,8 +2,43 @@
 var wins = 0;
 var losses = 0;
 var gameOver = false;
+var attackNumber = 1;
 
 //Character Objects (Status: Hero, Enemies, Defender, Defeated)
+var chewbacca = {
+    name = "Chewbacca",
+    hp = 100,
+    attack = 9,
+    // image =
+    status = none,
+}
+
+var vadar = {
+    name = "Darth Vadar",
+    hp = 110,
+    attack = 8,
+    // image =
+    status = none,
+}
+
+var atAt = {
+    name = "All Terrain Armored Transport",
+    hp = 120,
+    attack = 7,
+    // image =
+    status = none,
+}
+
+var wicket = {
+    name = "Wickeet",
+    hp = 90,
+    attack = 10,
+    // image =
+    status = none,
+}
+
+//Place Character Options to hero section
+
 
 //Choose Hero
 
