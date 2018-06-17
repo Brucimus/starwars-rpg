d5983e5a669bff937ef90da2b538f534db67297f
Brucimus <Brucimus@gmail.com>
2018-06-17 11:41:02 -0700

    initial commit with comments

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+//Win, Loss, is Game Over? Variables
+
+//Character Objects (Status: Hero, Enemies, Defender, Defeated)
+
+//Choose Hero
+
+    //Moves other hero options to Enemies Section
+
+//Choose Defender
+
+    //Moves Defender to Defender and leaves the others in Enemies Section
+
+//If all Enemies Defeated Add Win
+
+//If Player Loses All Health Add Loss
+
+//Attack Button, Player Attacks, adds hit strength and decrease Defender HP
+
+    //If Defender Still has HP, attack hero
+
+//Defeated Defender is removed and player asked to pick another defender
